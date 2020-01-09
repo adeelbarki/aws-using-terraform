93d0c4ccdf003fbaaf01ed79da70545e03f1ce2e
Adeel Ahmed Khan <adeelbarki@gmail.com>
2020-01-09 15:57:26 +0500

    added networking to root module

--- main.tf
+++ main.tf
@@ -8,3 +8,10 @@ module "storage" {
   project_name = "${var.project_name}" 
 }
 
+# Deploy Nerworking Resources
+module "networking" {
+  source = "./networking"
+  vpc_cidr = "${var.vpc_cidr}"
+  public_cidrs = "${var.public_cidrs}"
+  accessip = "${var.accessip}"
+}
