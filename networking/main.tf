1645341a2d5e807306071af1cd5855b9f797b5b5
Adeel Ahmed Khan <adeelbarki@gmail.com>
2020-01-09 12:31:04 +0500

    correction in resource rt-association for count

--- networking/main.tf
+++ networking/main.tf
@@ -54,7 +54,7 @@ resource "aws_subnet" "tf_public_subnet" {
 }
 
 resource "aws_route_table_association" "tf_public_assoc" {
-  count = "${aws_subnet.tf_public_subnet.count}"
+  count = "${length(aws_subnet.tf_public_subnet)}"
   subnet_id = "${aws_subnet.tf_public_subnet.*.id[count.index]}"
   route_table_id = "${aws_route_table.tf_public_rt.id}"
 }
