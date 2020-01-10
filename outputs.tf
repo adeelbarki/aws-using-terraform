b2df8103a83d50385a69d2067b1a13cb33e62870
Adeel Ahmed Khan <adeelbarki@gmail.com>
2020-01-10 16:15:09 +0500

    added outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,28 @@
+#-----Root outputs-----------------------
+
 #--------Storage/outputs.tf---------------
 output "bucket-name" {
   value = "${module.storage.bucketname}"
+}
+
+#-------Network outputs-------------------
+output "public-subnets" {
+  value = "${join(", ", module.networking.public_subnets)}"
+}
+
+output "subnet_ips" {
+  value = "${join(", ", module.networking.subnet_ips)}"
+}
+
+output "public_security_group" {
+  value = "${module.networking.public_sg}"
+}
+
+#-------Compute Outputs-------------------
+output "public_instance_ids" {
+  value = "${module.compute.server_id}"
+}
+
+output "public_instance_ips" {
+  value = "${module.compute.server_ip}"
 }
